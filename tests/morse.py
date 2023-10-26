dfbdbfb05eed3011b0df9bd96fa05239e932e40e
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-10-26 23:11:06 +0300

    Done 1 task

--- tests/morse.py
+++ tests/morse.py
@@ -1,53 +1,93 @@
 """Morse Code Translator"""
 
 LETTER_TO_MORSE = {
-    'A': '.-', 'B': '-...', 'C': '-.-.',
-    'D': '-..', 'E': '.', 'F': '..-.',
-    'G': '--.', 'H': '....', 'I': '..',
-    'J': '.---', 'K': '-.-', 'L': '.-..',
-    'M': '--', 'N': '-.', 'O': '---',
-    'P': '.--.', 'Q': '--.-', 'R': '.-.',
-    'S': '...', 'T': '-', 'U': '..-',
-    'V': '...-', 'W': '.--', 'X': '-..-',
-    'Y': '-.--', 'Z': '--..', '1': '.----',
-    '2': '..---', '3': '...--', '4': '....-',
-    '5': '.....', '6': '-....', '7': '--...',
-    '8': '---..', '9': '----.', '0': '-----',
-    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
-    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
-    ' ': ' '
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    ", ": "--..--",
+    ".": ".-.-.-",
+    "?": "..--..",
+    "/": "-..-.",
+    "-": "-....-",
+    "(": "-.--.",
+    ")": "-.--.-",
+    " ": " ",
 }
 
-MORSE_TO_LETTER = {
-    morse: letter
-    for letter, morse in LETTER_TO_MORSE.items()
-}
+MORSE_TO_LETTER = {morse: letter for letter, morse in LETTER_TO_MORSE.items()}
 
 
 def encode(message: str) -> str:
     """
     Кодирует строку в соответсвие с таблицей азбуки Морзе
-    """
-    encoded_signs = [
-        LETTER_TO_MORSE[letter] for letter in message
-    ]
 
-    return ' '.join(encoded_signs)
+    >>> encode('HELLO THERE')
+    '.... . .-.. .-.. ---   - .... . .-. .'
+    >>> encode('!HOLA')
+    Traceback (most recent call last):
+    ...
+    KeyError: '!'
+
+    А это мы пропустим
+    >>> encode('please write your message in UPPERCASE') # doctest: +SKIP
+
+    Абьюз точек и бесконечная любовь к функциям
+    >>> list(map(encode, ['AB', 'TEXT', 'CD'])) # doctest: +ELLIPSIS
+    ['.- -...',\t...,        '-.-. -..']
+
+    Надеюсь, не пригодится, но лучше знать
+    >>> encode('SOS')
+    '... --- ...'
+    """
+    encoded_signs = [LETTER_TO_MORSE[letter] for letter in message]
+
+    return " ".join(encoded_signs)
 
 
 def decode(morse_message: str) -> str:
     """
     Декодирует строку из азбуки Морзе в английский
     """
-    decoded_letters = [
-        MORSE_TO_LETTER[letter] for letter in morse_message.split()
-    ]
+    decoded_letters = [MORSE_TO_LETTER[letter] for letter in morse_message.split()]
 
-    return ''.join(decoded_letters)
+    return "".join(decoded_letters)
 
 
-if __name__ == '__main__':
-    morse_msg = '-- .- .. -....- .--. -.-- - .... --- -. -....- ..--- ----- .---- ----.'
+if __name__ == "__main__":
+    morse_msg = "-- .- .. -....- .--. -.-- - .... --- -. -....- ..--- ----- .---- ----."
     decoded_msg = decode(morse_msg)
     print(decoded_msg)
     assert morse_msg == encode(decoded_msg)
